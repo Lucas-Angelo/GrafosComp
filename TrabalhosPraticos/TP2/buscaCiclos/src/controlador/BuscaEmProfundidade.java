54320cce68abd3ae35fc7d2f84f25308e0672813
lcs2001 <lcs2001_@hotmail.com>
2021-05-05 11:53:46 -0300

    Permutacao

--- TrabalhosPraticos/TP2/buscaCiclos/src/controlador/BuscaEmProfundidade.java
+++ TrabalhosPraticos/TP2/buscaCiclos/src/controlador/BuscaEmProfundidade.java
@@ -27,20 +27,49 @@ public class BuscaEmProfundidade {
         init(arestas);
     }
 
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\nCiclos por busca em profundidade:\n");
+        for (int i=0; i<this.ciclos.getArco().size(); i++) {
+            int ciclo[] = this.ciclos.getArco().get(i);
+            stringBuilder.append(i+1 + "º" + " ciclo: " + ciclo[0]);
+
+            for (int j = 1; j < ciclo.length; j++) {
+                stringBuilder.append(" - " + ciclo[j]);
+            }
+            
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    // Função que verifica se dois caminhos são iguais
+    private boolean equals(int[] caminhoUm, int[] caminhoDois) {
+        boolean igual = (caminhoUm[0] == caminhoDois[0]) && (caminhoUm.length == caminhoDois.length);
+
+        for (int i = 1; i < caminhoUm.length && igual; i++) {
+            if (caminhoUm[i] != caminhoDois[i]) {
+                igual = false;
+            }
+        }
+
+        return igual;
+    }
+
     private void buscarCiclos(int[] caminho) {
             int verticeUm = caminho[0];
             int verticeTratado;
             int[] caminhoDerivado= new int[caminho.length + 1];
 
             for (int i = 0; i < this.arcos.length; i++) {
-                for (int y = 0; y <= 1; y++) {
-                    if (this.arcos[i][y] == verticeUm) { // se o vértice do grafo atual é o mesmo que o primeiro do caminho
-                        verticeTratado = this.arcos[i][(y + 1) % 2];
+                for (int j = 0; j <= 1; j++) {
+                    if (this.arcos[i][j] == verticeUm) { // se o vértice do grafo atual é o mesmo que o primeiro do caminho
+                        verticeTratado = this.arcos[i][(j + 1) % 2];
                         if (!foiVisitado(verticeTratado, caminho)) { // se o vértice vizinho/adjacente não está no caminho
                             caminhoDerivado[0] = verticeTratado;
                             // A posição 0 ocupado com o vértice tratado e o resto inserido a partir do index 1
-                            for (int j = 0; j < caminho.length; j++) {
-                                caminhoDerivado[j + 1] = caminho[j];
+                            for (int k = 0; k < caminho.length; k++) {
+                                caminhoDerivado[k + 1] = caminho[k];
                             }
                             // Recursividade para buscar os caminhos dentro do caminho atual
                             buscarCiclos(caminhoDerivado);
@@ -56,17 +85,30 @@ public class BuscaEmProfundidade {
             }
     }
 
-    // Função que verifica se dois caminhos são iguais
-    private boolean equals(int[] caminhoUm, int[] caminhoDois) {
-        boolean igual = (caminhoUm[0] == caminhoDois[0]) && (caminhoUm.length == caminhoDois.length);
+    //  Função que compara um caminho com os ciclos já encontrados e retorna true se for novo
+    private boolean caminhoNovo(int[] caminho) {
+        boolean novo = true;
 
-        for (int i = 1; i < caminhoUm.length && igual; i++) {
-            if (caminhoUm[i] != caminhoDois[i]) {
-                igual = false;
+        for(int i = 0; i< this.ciclos.getArco().size() && novo==true; i++) {
+            if (equals(this.ciclos.getArco().get(i), caminho)) {
+                novo = false;
             }
         }
 
-        return igual;
+        return novo;
+    }
+
+    // Função que verifica se um vértice qualquer já faz parte de algum caminho, ou seja, já foi visitado.
+    private boolean foiVisitado(int vertice, int[] caminho) {
+        boolean visitado = false;
+        
+        for (int i=0; i<caminho.length && visitado==false; i++) {
+            if (caminho[i] == vertice) {
+                visitado = true;
+            }
+        }
+        
+        return visitado;
     }
 
     // Função que cria um array inverso de um caminho, colocar na primeira posição o menor identificador de vértice
@@ -103,19 +145,6 @@ public class BuscaEmProfundidade {
         return caminhoOrdenado;
     }
 
-    //  Função que compara um caminho com os ciclos já encontrados e retorna true se for novo
-    private boolean caminhoNovo(int[] caminho) {
-        boolean novo = true;
-
-        for(int i = 0; i< this.ciclos.getArco().size() && novo==true; i++) {
-            if (equals(this.ciclos.getArco().get(i), caminho)) {
-                novo = false;
-            }
-        }
-
-        return novo;
-    }
-
     // Função que procura o menor identificador de vértice em um caminho, usado para ordenar pelo menor
     private int procurarMenor(int[] caminho) {
         int menor = caminho[0];
@@ -129,33 +158,4 @@ public class BuscaEmProfundidade {
         return menor;
     }
     
-    // Função que verifica se um vértice qualquer já faz parte de algum caminho, ou seja, já foi visitado.
-    private boolean foiVisitado(int vertice, int[] caminho) {
-        boolean visitado = false;
-        
-        for (int i=0; i<caminho.length && visitado==false; i++) {
-            if (caminho[i] == vertice) {
-                visitado = true;
-            }
-        }
-        
-        return visitado;
-    }
-    
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Ciclos por busca em profundidade:\n");
-        for (int i=0; i<this.ciclos.getArco().size(); i++) {
-            int ciclo[] = this.ciclos.getArco().get(i);
-            stringBuilder.append(i+1 + "º" + " ciclo: " + ciclo[0]);
-
-            for (int j = 1; j < ciclo.length; j++) {
-                stringBuilder.append(" - " + ciclo[j]);
-            }
-            
-            stringBuilder.append("\n");
-        }
-        return stringBuilder.toString();
-    }
-
 }
